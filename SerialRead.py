887397092124ce987138c9e6a1071a6fc810b1c0
Chen Pei Hao <s894117@gmail.com>
2018-05-16 17:59:47 +0900

    measure time of serail read select port into

--- SerialRead.py
+++ SerialRead.py
@@ -3,6 +3,7 @@ import logging
 import serial.tools.list_ports
 import pandas as pd
 from functools import reduce
+import time
 
 def serial_connect(port, baudrate=19200):
     ser = serial.Serial()
@@ -27,12 +28,15 @@ def serial_read(port,save_c, baudrate=19200):
 
     while True:
         if count < save_c * 3:
+            start_time = time.time()
             data = arduino.readline().decode('UTF-8')
+            end_time = time.time()
+            print("The time of measurement: {}".format(end_time - start_time))
             list_data = data_process(data)
             print(list_data)
             if list_data[0] in result_d.keys():
                 count = count+1
-                result_d[list_data[0]].append(list_data[1])
+                result_d[list_data[0]].append(float(list_data[1]))
         else:
             break
     # print("size of X : {}".format(len(result_d['X'])))
@@ -77,13 +81,13 @@ def distance_from_avg(int_list):
 def diff_cal(d_data):
     return reduce(lambda x, y: x + y, map(distance_from_avg, d_data.values())) ** 0.5
 
-def main(file_name):
+def read_all_data():
+    pass
+
+
+
+def connect_ardunio():
     port = list_port()
-    result_dict = {}
-    result_dict['distance'] = []
-    result_dict['var'] = []
-    result_dict['X'] = []
-    result_dict['Y'] = []
     if not port:
         raise IOError("No Arduino found")
     elif len(port) > 1:
@@ -97,6 +101,16 @@ def main(file_name):
         use_port = port[port_select]
     else:
         use_port = port[0]
+    return use_port
+
+def main(file_name):
+    use_port = connect_ardunio()
+    result_dict = {}
+    result_dict['distance'] = []
+    result_dict['var'] = []
+    result_dict['X'] = []
+    result_dict['Y'] = []
+    result_dict['Z'] = []
     if serial_connect(use_port):
         # Enter and calculate the distance
         while True:
@@ -105,17 +119,22 @@ def main(file_name):
                 break
             elif len(key_input) == 0:
                 continue
-            x, y = int(key_input.split(',')[0]), int(key_input.split(',')[1])
-            distance = (x ** 2 + y ** 2) ** 0.5
+            item_key_input = list(map(int, key_input.split(',')))
+            x, y, z = item_key_input[0], item_key_input[1], item_key_input[2]
+            distance = (x ** 2 + y ** 2 + z ** 2) ** 0.5
             # Read the data from serial and do some process
             dict_data = serial_read(use_port, 50)
             var = diff_cal(dict_data)
+            key_input = input("Valid Value or Not ? [y/n]")
+            if key_input != 'y' or 'Y':
+                continue
             # Save the data into dictionary
             result_dict['distance'].append(distance)
             result_dict['var'].append(var)
             result_dict['X'].append(x)
             result_dict['Y'].append(y)
-        df = pd.DataFrame(result_dict, columns=['X', 'Y', 'R', 'Var'])
+            result_dict['Z'].append(z)
+        df = pd.DataFrame(result_dict, columns=['X', 'Y', 'Z', 'distance', 'var'])
         df.to_csv('./result/{}.csv'.format(file_name))
     # df = pd.DataFrame(dict_data, columns=['X', 'Y', 'Z'])
     # df.to_csv('./result/{}.csv'.format(file_name))
@@ -134,5 +153,5 @@ def test():
     data = input("Enter the word")
     print(data.lower())
 if __name__ == '__main__':
-    # main('5_90')
-    test()
+    main('test')
+    # test()
