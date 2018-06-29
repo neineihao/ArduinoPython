f91fb7696712bebd08cf316081c08eb07c86422d
Chen Pei Hao <s894117@gmail.com>
2018-06-29 14:19:12 +0900

    heatmap complete

--- error_analysis.py
+++ error_analysis.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 
 STD = 0.21
 
-
 def distance2signal(d):
     co = 3026.88441
     signal = co * d ** (-3)
@@ -58,21 +57,56 @@ def position_variety(distance_matrix):
         result[i] = distance_variety(distance_matrix[i])
     return result
 
-def plane_simulation(point):
-    x, y = 20, 20
-    place = np.zeros((y,x))
-    z = 5
-    for x in range(20):
-        for y in range(20):
-            p = np.array([x-10, y-10, z])
+def plane_simulation(point,size,z):
+    x_i, y_i = size, size
+    place = np.zeros((y_i,x_i))
+    for x in range(x_i):
+        for y in range(y_i):
+            x_c = int(x - x_i / 2)
+            y_c = int(y - y_i / 2)
+            p = np.array([x_c, y_c, z])
             place[y,x] = distance_error(p, point)
     return place
 
-if __name__ == '__main__':
-    position = np.array([5,8,9])
-    point = np.array([(5, 0, 0), (-5, 0, 0), (0, -5, 0)])
-    print("distance error is : {}".format(distance_error(position, point)))
-    z = 3
-    heat_map = plane_simulation(point)
-    plt.imshow(heat_map, interpolation='nearest')
+def heat_map(x, xlabel, ylabel):
+    if x.shape[1] != len(xlabel) or x.shape[0] != len(ylabel):
+        raise ValueError("The size is not match")
+    fig, ax = plt.subplots()
+    im = ax.imshow(x)
+    # We want to show all ticks...
+    ax.set_xticks(np.arange(len(xlabel)))
+    ax.set_yticks(np.arange(len(ylabel)))
+    # ... and label them with the respective list entries
+    ax.set_xticklabels(xlabel)
+    ax.set_yticklabels(ylabel)
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    for i in range(len(ylabel)):
+        for j in range(len(xlabel)):
+            text = ax.text(j, i, round(x[i, j], 2),
+                           ha="center", va="center", color="w")
+
+    ax.set_title("The heatmap in the plane when z = {}".format(z))
+    fig.tight_layout()
     plt.show()
+
+
+if __name__ == '__main__':
+    z = 5
+    test_time = 50
+    np_size = 11
+    point = np.array([(5, 0, 0), (-5, 0, 0), (0, -5, 0)])
+    place = np.zeros((np_size,np_size))
+    for i in range(test_time):
+        place += plane_simulation(point, np_size, z)
+    place = place / test_time
+    xlabel = [x-5 for x in range(np_size)]
+    ylabel = [y-5 for y in range(np_size)]
+    # print("The size of xlabel: {}".format(len(xlabel)))
+    # print("The size of ylabel: {}".format(len(ylabel)))
+    # print("The size of data: {}".format(place.shape[1]))
+    heat_map(place, xlabel, ylabel)
